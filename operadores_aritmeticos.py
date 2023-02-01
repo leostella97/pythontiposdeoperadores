608feca329aecae63b194d821adcca365ee64a6a
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-02-01 12:36:25 -0300

    Operadores aritméticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+print("Adição 1+1:", 1+1)
+print("Subtração 10-2:", 10-2)
+print("Multiplicação 4*3:", 4*3)
+print("Divisão 12/3:", 12/3)
+print("Divisão retornado em inteiro 12//2:", 12//2)
+print("Módulo 10%3:", 10%3)
+print("Exponenciação 2**3: ", 2**3)
+#o Python também segue as definições da área de matemática como os exemplos a seguir
+print("10-5*2:", 10-5*2)
+print("(10-5)*2):", (10-5)*2)
+print("10**2*2:", 10**2*2)
+print("10**(2*2):", 10**(2*2))
+print("10/2*4:",10/2*4, end="\n\n")
+
+print("###PRODUTOS###")
+produto_1 = 20
+produto_2 = 10
+print("Produto 1 + produto 2:", produto_1+produto_2)
+print("Produto 1 - produto 2:", produto_1-produto_2)
+print("Produto 1 * produto 2:", produto_1*produto_2)
+print("Produto 1 ** produto 2:", produto_1**produto_2)
+print("Produto 1 % produto 2:", produto_1%produto_2)
+print("Produto 1 / produto 2:", produto_1/produto_2)
+print("Produto 1 // produto 2:", produto_1//produto_2)
