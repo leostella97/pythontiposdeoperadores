f7e59e83e5a51bf4c83a19098c007561d5cce533
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-02-01 12:37:56 -0300

    Operadores de comparação

---
+++ operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 450
+saque = 250
+
+print(saldo == saque) #igual
+print(saldo != saque) #diferente
+print(saldo > saque) #maior que
+print(saldo < saque) #menor que
+print(saldo >= saque) #maior ou igual
+print(saldo <= saque) #menor ou igual
